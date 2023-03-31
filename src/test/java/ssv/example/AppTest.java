38d10be669e06f2b024a660720adc985730c19fd
UBB <ubb@tests-MacBook-Pro.local>
2023-03-31 13:47:21 +0300

    add a test

--- src/test/java/ssv/example/AppTest.java
+++ src/test/java/ssv/example/AppTest.java
@@ -1,38 +1,70 @@
 package ssv.example;
+import static org.junit.Assert.assertTrue;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import domain.Student;
+import domain.Tema;
+import org.junit.Test;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
+public class AppTest
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
+    StudentValidator studentValidator = new StudentValidator();
+    TemaValidator temaValidator = new TemaValidator();
+    String filenameStudent = "fisiere/Studenti.xml";
+    String filenameTema = "fisiere/Teme.xml";
+    String filenameNota = "fisiere/Note.xml";
+    StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+    TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+    NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+    NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+    Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    @Test
+    public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void addStudentTest1() {
+        try {
+            Student stud = new Student("88","abc", -1, "loranz@loranz");
+            assert service.addStudent(stud) == stud;
+        }catch (ValidationException e) {
+            assert e.toString().equals("validation.ValidationException: Grupa incorecta!");
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addStudentTest2() {
+        try {
+            Student stud = new Student("","abc", -1, "loranz@loranz");
+            assert service.addStudent(stud) == stud;
+        }catch (ValidationException e) {
+            assert e.toString().equals("validation.ValidationException: Id incorect! Grupa incorecta!");
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void addStudentTest3() {
+        try {
+            Student stud = new Student("88","", -1, "loranz@loranz");
+            assert service.addStudent(stud) == stud;
+        }catch (ValidationException e) {
+            assert e.toString().equals("validation.ValidationException: Nume incorect! Grupa incorecta!");
+            e.printStackTrace();
+        }
     }
 }
